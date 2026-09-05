2de836cd0b5227d6df8d27f07b1ff7e51638b43d
varunsai2409 <varunsaithokala2008@gmail.com>
2026-09-05 23:23:49 +0530

    feat: add string Class problem 4

---
+++ string/class_problems/ClassProblem4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ClassProblem4{
+    static char findFirstNonRepeatingChar(String text) {
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : text.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        for (char c : text.toCharArray()) {
+            if (freq.get(c) == 1) return c;
+        }
+        return '\0'; // No non-repeating character
+    }
+
+    public static void main(String[] args) {
+        String input = "swiss";
+        char result = findFirstNonRepeatingChar(input);
+        if (result == '\0') System.out.println("No Non-Repeating Character Found");
+        else System.out.println("First Non-Repeating Character: '" + result + "'");
+    }
+}

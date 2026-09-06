f6d3256961a4835478f08e5d049ab0b53d3e262d
varunsai2409 <varunsaithokala2008@gmail.com>
2026-09-06 20:05:49 +0530

    feat: add string_operation assignment problem 3

---
+++ string_operations/assignment_problems/AssignmentProblem3.java
@@ -0,0 +1,17 @@
+public class AssignmentProblem3{
+    public static void parseInventoryRecord(String csvLine) {
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+            return;
+        }
+
+        System.out.println("Product: " + fields[0] + " | SKU: " + fields[1] + " | Qty: " + fields[2]);
+    }
+
+    public static void main(String[] args) {
+        parseInventoryRecord("Wireless Mouse,WM-2201,150");
+        parseInventoryRecord("Wireless Mouse,150");
+    }
+}
